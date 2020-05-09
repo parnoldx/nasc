d3d2e0252b965fd0ffc1524c97eafb7d4afcae2e
parnold-x <parnold1@gmail.com>
2020-05-09 14:19:23 +0200

    move to meson
    Autor:           parnold-x <parnold1@gmail.com>

---
+++ meson/post_install.py
@@ -0,0 +1,21 @@
+import argparse
+import os
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--schemadir", action="store", required=True)
+parser.add_argument("--iconsdir", action="store", required=True)
+
+args = vars(parser.parse_args())
+
+schemadir = args["schemadir"]
+iconsdir = args["iconsdir"]
+
+hicolordir = os.path.join(iconsdir, 'hicolor')
+
+if not os.environ.get('DESTDIR'):
+    print('Compiling gsettings schemas ...')
+    subprocess.run(['glib-compile-schemas', schemadir])
+
+    print('Compiling icon cache ...')
+    subprocess.run(['gtk-update-icon-cache', hicolordir])
